c40f195c0907945680d7ec14cb76cc47d5c13460
Shreya <shreyatiwari587@gmail.com>
2021-09-08 17:35:20 -0700

    first commit

--- app/src/main/java/com/example/ble/MainActivity.java
+++ app/src/main/java/com/example/ble/MainActivity.java
@@ -215,6 +215,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public String getTextContent() throws DOMException {
+                System.out.println("dgfhkd");
                 return null;
             }
 
